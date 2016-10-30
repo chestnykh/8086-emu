04438723bc084dd1afa9d96f8615328a9bb496bc
chestnykh <dm.chestnykh@gmail.com>
2016-10-30 20:01:24 +0300

    The structure presents CPU registers is changed

---
+++ cpu.cpp
@@ -0,0 +1,11 @@
+#include <cpu.h>
+
+Cpu::Cpu()
+{
+
+}
+
+Cpu::~Cpu()
+{
+
+}
--- include/cpu.h
+++ include/cpu.h
@@ -5,15 +5,42 @@
 #include "instructions.h"
 
 
-typedef unsigned char uchar;
-
+extern unsigned char memory[1<<20];
 
 struct cpuregs
 {
-	uint16_t ax;
-	uint16_t bx;
-	uint16_t cx;
-	uint16_t dx;
+	union{
+		uint16_t ax;
+		struct{
+			uint8_t al;
+			uint8_t ah;
+		};
+	} ax;
+
+	union{
+		uint16_t bx;
+		struct{
+			uint8_t bl;
+			uint8_t bh;
+		};
+	} bx;
+
+	union{
+		uint16_t cx;
+		struct{
+			uint8_t cl;
+			uint8_t ch;
+		};
+	} cx;
+	
+	union{
+		uint16_t dx;
+		struct{
+			uint8_t dl;
+			uint8_t dh;
+		};
+	} dx;
+
 	uint16_t sp;
 	uint16_t bp;
 	uint16_t si;
@@ -28,15 +55,38 @@ struct cpuregs
 };
 
 
+/*The numbers that can identify register in REG and R/M fields of instruction
+ The MOD field tells us what register in pair is used 
+ */
 
-/*
-class  Cpu : public InstructionParser
+enum regCodes{
+	AL = 0,
+	AX = 0,
+	CL = 1,
+	CX = 1,
+	DL = 2,
+	DX = 2,
+	BL = 3,
+	BX = 3,
+	AH = 4,
+	SP = 4,
+	CH = 5,
+	BP = 5,
+	DH = 6,
+	SI = 6,
+	BH = 7,
+	DI = 7
+};
+
+
+class Cpu : public InstructionParser
 {
 private:
-	struct cpuregs regs;
 
 public:
-
+	struct cpuregs regs;
+	Cpu(struct cpuregs startState) : InstructionParser(){};
+	~Cpu(){};
 };
-*/
+
 
