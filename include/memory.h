13aac5001e4b431212e84afd85fd11091ca0f452
chestnykh <dm.chestnykh@gmail.com>
2016-11-02 02:21:05 +0300

    Memory representstion is changed

--- include/memory.h
+++ include/memory.h
@@ -1,4 +1,16 @@
 #pragma once
 
+typedef union address{
+	uint16_t word;
+	struct{
+		unsigned char lowtByte;
+		unsigned char highByte;	
+	};
+} address_t;
 
-unsigned char memory[1<<20];
+
+/*
+ в регистре ip содержится число - номер ячейки памяти
+ (смещение от базы - от cs)
+ */
+
