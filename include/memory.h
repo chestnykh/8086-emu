26b56247a9796b0d891c39a4ab769c584840172b
chestnykh <dm.chestnykh@gmail.com>
2016-11-07 00:44:42 +0300

    The representation of memory is changed

--- include/memory.h
+++ include/memory.h
@@ -1,13 +1,15 @@
 #pragma once
 
-typedef union address{
+typedef union memUnit{
 	uint16_t word;
 	struct{
 		unsigned char lowByte;
 		unsigned char highByte;	
 	};
-} addr_t;
+} mem_t;
 
+
+#define PA(addr) (this -> cpuregs.cs << 4 + this -> cpuregs.ip)
 
 /*
  в регистре ip содержится число - номер ячейки памяти
