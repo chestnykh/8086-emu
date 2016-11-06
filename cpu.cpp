8c5861e0926668434804450d71fee22dbe8bc3ea
chestnykh <dm.chestnykh@gmail.com>
2016-11-06 20:10:23 +0300

    InstructionParser is nested in Cpu class

--- cpu.cpp
+++ cpu.cpp
@@ -1,11 +1,10 @@
 #include <cpu.h>
 #include "memory.h"
 
-Cpu::Cpu(addr_t *instrAddr) : InstructionParser(instrAddr)
+Cpu::Cpu(addr_t *instrAddr)
 {
-
+	Cpu::Parser = new InstructionParser(instrAddr);
 }
-
 
 Cpu::~Cpu()
 {
--- include/cpu.h
+++ include/cpu.h
@@ -78,13 +78,109 @@ enum regCodes{
 	DI = 7
 };
 
+class Cpu;
+typedef void (Cpu::*InstructionGroupHandler_t)(addr_t *);
 
-class Cpu : public InstructionParser
-{
+class Cpu{
+
 private:
+	struct cpuregs regs;
+
+
+
+	void ADD_GroupHandler(addr_t *addr);
+	void PUSH_GroupHandler(addr_t *addr);
+	void POP_GroupHandler(addr_t *addr);
+	void OR_GroupHandler(addr_t *addr);
+	void ADC_GroupHandler(addr_t *addr);
+	void SBB_GroupHandler(addr_t *addr);
+	void AND_GroupHandler(addr_t *addr);
+	void DAA_GroupHandler(addr_t *addr);
+	void SUB_GroupHandler(addr_t *addr);
+	void DAS_GroupHandler(addr_t *addr);
+	void XOR_GroupHandler(addr_t *addr);
+	void AAA_GroupHandler(addr_t *addr);
+	void CMP_GroupHandler(addr_t *addr);
+	void AAS_GroupHandler(addr_t *addr);
+	void INC_GroupHandler(addr_t *addr);
+	void DEC_GroupHandler(addr_t *addr);
+	void JMP_GroupHandler(addr_t *addr); /*all jumps except jcxz*/
+	void TEST_GroupHandler(addr_t *addr);
+	void XCHG_GroupHandler(addr_t *addr);
+	void MOV_GroupHandler(addr_t *addr);
+	void LEA_GroupHandler(addr_t *addr);
+	void NOP_GroupHandler(addr_t *addr);
+	void CBW_GroupHandler(addr_t *addr);
+	void CWD_GroupHandler(addr_t *addr);
+	void CALL_GroupHandler(addr_t *addr);
+	void WAIT_GroupHandler(addr_t *addr);
+	void SAHF_GroupHandler(addr_t *addr);
+	void LAHF_GroupHandler(addr_t *addr);
+	void MOVS_GroupHandler(addr_t *addr);
+	void CMPS_GroupHandler(addr_t *addr);
+	void STOS_GroupHandler(addr_t *addr);
+	void LODS_GroupHandler(addr_t *addr);
+	void SCAS_GroupHandler(addr_t *addr);
+	void RET_GroupHandler(addr_t *addr);
+	void LES_GroupHandler(addr_t *addr);
+	void LDS_GroupHandler(addr_t *addr);
+	void INT_GroupHandler(addr_t *addr);
+	void INTO_GroupHandler(addr_t *addr);
+	void IRET_GroupHandler(addr_t *addr);
+	void ROL_GroupHandler(addr_t *addr);
+	void ROR_GroupHandler(addr_t *addr);
+	void RCL_GroupHandler(addr_t *addr);
+	void RCR_GroupHandler(addr_t *addr);
+	void SAL_SHL_GroupHandler(addr_t *addr);
+	void SHR_GroupHandler(addr_t *addr);
+	void SAR_GroupHandler(addr_t *addr);
+	void AAM_GroupHandler(addr_t *addr);
+	void AAD_GroupHandler(addr_t *addr);
+	void XLAT_GroupHandler(addr_t *addr);
+	void ESC_GroupHandler(addr_t *addr);
+	void LOOPNE_LOOPNZ_GroupHandler(addr_t *addr);
+	void LOOPE_LOOPZ_GroupHandler(addr_t *addr);
+	void LOOP_GroupHandler(addr_t *addr);
+	void JCXZ_GroupHandler(addr_t *addr);
+	void IN_GroupHandler(addr_t *addr);
+	void OUT_GroupHandler(addr_t *addr);
+	void LOCK_GroupHandler(addr_t *addr); /*prefix*/
+	void REPNE_REPNZ_GroupHandler(addr_t *addr);
+	void REP_REPE_REPZ_GroupHandler(addr_t *addr);
+	void HLT_GroupHandler(addr_t *addr);
+	void CMC_GroupHandler(addr_t *addr);
+	void NOT_GroupHandler(addr_t *addr);
+	void NEG_GroupHandler(addr_t *addr);
+	void MUL_GroupHandler(addr_t *addr);
+	void IMUL_GroupHandler(addr_t *addr);
+	void DIV_GroupHandler(addr_t *addr);
+	void IDIV_GroupHandler(addr_t *addr);
+	void CLC_GroupHandler(addr_t *addr);
+	void STC_GroupHandler(addr_t *addr);
+	void CLI_GroupHandler(addr_t *addr);
+	void STI_GroupHandler(addr_t *addr);
+	void CLD_GroupHandler(addr_t *addr);
+	void STD_GroupHandler(addr_t *addr);
+
+	void GRP_80_GroupHandler(addr_t *addr);
+	void GRP_81_GroupHandler(addr_t *addr);
+	void GRP_82_GroupHandler(addr_t *addr);
+	void GRP_83_GroupHandler(addr_t *addr);
+	void GRP_D0_GroupHandler(addr_t *addr);
+	void GRP_D1_GroupHandler(addr_t *addr);
+	void GRP_D2_GroupHandler(addr_t *addr);
+	void GRP_D3_GroupHandler(addr_t *addr);
+	void GRP_F6_GroupHandler(addr_t *addr);
+	void GRP_F7_GroupHandler(addr_t *addr);
+	void GRP_FE_GroupHandler(addr_t *addr);
+	void GRP_FF_GroupHandler(addr_t *addr);
+	void segOverridePrefixHandler(addr_t *addr);
+
 
 public:
-	struct cpuregs regs;
+	friend class InstructionParser;
+	InstructionParser *Parser;
+
 	Cpu(addr_t *instrAddr);
 	~Cpu();
 };
