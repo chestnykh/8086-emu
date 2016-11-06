ba68faa885e6cab14d59d6dd35e06f9072b1511e
chestnykh <dm.chestnykh@gmail.com>
2016-11-06 20:07:21 +0300

    this is the file which with functions emulating CPU work

---
+++ cpuexec.cpp
@@ -0,0 +1,434 @@
+#include <cpu.h>
+
+
+void Cpu::AAD_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::GRP_80_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::GRP_81_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::GRP_82_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+
+void Cpu::GRP_83_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::GRP_D0_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::GRP_D1_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::GRP_D2_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::GRP_D3_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::GRP_F6_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::GRP_F7_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::GRP_FE_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::GRP_FF_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::segOverridePrefixHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::ADD_GroupHandler(addr_t *addr)
+{
+	return ;	
+}
+
+void Cpu::PUSH_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::POP_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::OR_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::ADC_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::SBB_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::AND_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::DAA_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::SUB_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::DAS_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::XOR_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::AAA_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::CMP_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::AAS_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::INC_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::DEC_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::JMP_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::TEST_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::XCHG_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::MOV_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::LEA_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::NOP_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::CBW_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::CWD_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::CALL_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::WAIT_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::SAHF_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::LAHF_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::MOVS_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::CMPS_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::STOS_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::LODS_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::SCAS_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::RET_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::LES_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::LDS_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::INT_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::INTO_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::IRET_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::ROL_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::ROR_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::RCL_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::RCR_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::SAL_SHL_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::SHR_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::SAR_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::AAM_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::XLAT_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::ESC_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::LOOPNE_LOOPNZ_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::LOOPE_LOOPZ_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::LOOP_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::JCXZ_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::IN_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::OUT_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::LOCK_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::REPNE_REPNZ_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::REP_REPE_REPZ_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::HLT_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::CMC_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::NOT_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::NEG_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::MUL_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::IMUL_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::DIV_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::IDIV_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::CLC_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::STC_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::CLI_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::STI_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::CLD_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
+void Cpu::STD_GroupHandler(addr_t *addr)
+{
+	return ;
+}
+
