272c818b3533e5dd45d6dd646178c4f8e7dd3034
chestnykh <dm.chestnykh@gmail.com>
2016-11-07 00:44:54 +0300

    Little changes

--- main.cpp
+++ main.cpp
@@ -9,48 +9,25 @@
 using std::cout;
 
 
-addr_t memory[(1<<20)/2];
+mem_t memory[(1<<20)/2];
 
 
 
 
 int main(int argc, char *argv[])
 {
-	struct instruction i = 
-	{
-		.one = 2,
-		.two = 7,
-		.disp = 65535-255,
-		.data = 65535-255, 
-		.size = 6
-	};
 	struct cpuregs r = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 	std::ifstream inp(argv[1], std::ios::binary);
 	inp.seekg(0, inp.end);
 	int len = inp.tellg();
 	inp.seekg(0, inp.beg);
-	inp.read((char*)memory, len);
-	addr_t addr = {.word = 0xD7};
+	Cpu A(0, memory);
+	mem_t *p = memory;
+	memory[0].word = 0x0e;
+	InstructionGroupHandler_t h = A.Parser -> getGenericGroupHandler(p);
+	(A.*h)(NULL);
+	//A.Parser -> startByte -> word = 78;
+	//cout<<A.Parser -> startByte -> word<<'\n';
 
-
-	Cpu A(&addr);
-	InstructionGroupHandler_t h = A.Parser -> getGenericGroupHandler(&addr);
-	A.Parser -> startByte -> word = 78;
-	cout<<A.Parser -> startByte -> word<<'\n';
-	//unsigned char a = 0x03;
-	//void *p = (A.*f)(NULL);
-
-	/*
-	cout<<(int)OPCODE(i)<<'\n';
-	cout<<(int)D(i)<<'\n';
-	cout<<(int)W(i)<<'\n';
-	cout<<(int)MOD(i)<<'\n';
-	cout<<(int)REG(i)<<'\n';
-	cout<<(int)RM(i)<<'\n';
-	cout<<(int)LOW_DISP(i)<<'\n';
-	cout<<(int)HIGH_DISP(i)<<'\n';
-	cout<<(int)LOW_DATA(i)<<'\n';
-	cout<<(int)HIGH_DATA(i)<<'\n';
-	*/
 	return 0;
 }
